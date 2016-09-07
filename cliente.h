f739095af6433b8a9e01c35c007ca8d5a3e9abf1
ellioth <ellioth@ubuntu>
2016-09-07 09:20:02 -0600

    cliente listo, faltan pruebas

--- cliente.h
+++ cliente.h
@@ -7,13 +7,34 @@
 
 #ifndef CLIENTE_H
 #define	CLIENTE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <iostream>
+#include "Constantes.h"
 
-class cliente {
+using namespace std;
+class cliente :public Constantes{
 public:
-    cliente();
-    cliente(const cliente& orig);
+    cliente(int pPort, char* pIP);
     virtual ~cliente();
+    void* sendMessageToServer(string pMessage, int pLenght );
+    void* sendMessageToServer(string pMessage);
 private:
+    int _port;
+    char* _ip;
+    int _sockfd,_n;
+    struct sockaddr_in _serv_addr;
+    struct hostent * _server;
+    void * connectToServer(int port, const char* ip);
+    void error(const char* msg);
 
 };
 
