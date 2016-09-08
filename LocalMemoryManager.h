09169a8414560a49306daf09238756b481f35710
ellioth <ellioth@ubuntu>
2016-09-08 06:03:31 -0600

    cuerpos de los metodos momentaneos ya creados y esperando para pruebas

--- LocalMemoryManager.h
+++ LocalMemoryManager.h
@@ -9,21 +9,25 @@
 #define	LOCALMEMORYMANAGER_H
 #include "cliente.h"
 #include "xReference.h"
+#include "xReference.cpp"
 #include "Constantes.h"
-
+#include "JsonCreator.h"
+#include <new>
 
 class LocalMemoryManager : public Constantes{
 public:
     LocalMemoryManager();
     virtual ~LocalMemoryManager();
-    //xReference* xMalloc(int pSize, int pType);
-    //xReference* xMalloc(int pSize, int pType, void* pValue);
-    //void xAssing(xReference* pRefe, void* pValue);
-    //void xFree(xReference* pRefe);
+    char* initialize(int pPort, char* pIp);
+    char* requestNewToken();
+    void* xMalloc(int pSize, int pType);
+    void* xMalloc(int pSize, int pType, void* pValue);
+    void xAssing(void* pRefe, void* pValue, int pType);
+    void xFree(void* pRefe, int pType);
 private:
     cliente * _cliente;
-    
-
+    JsonCreator* _JsonMessageCreator;
+    char* _Token;
 };
 
 #endif	/* LOCALMEMORYMANAGER_H */
